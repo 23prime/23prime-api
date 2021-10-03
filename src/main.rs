228d93a7c51babe95911a9beb96d46cdb66b511b
23prime <23.prime.37@gmail.com>
2021-10-03 23:03:53 +0900

    Get log level from env

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use log::info;
 use std::env;
 
-use actix_web::middleware::Logger;
+use actix_web::middleware;
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 
 use anime_scraper::routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    env::set_var("RUST_LOG", "info");
-    info!("Set env RUST_LOG={}", env::var("RUST_LOG").unwrap());
+
+    if let Ok(rust_log) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", rust_log);
+    } else {
+        env::set_var("RUST_LOG", "debug");
+    }
+
+    info!("Log level: RUST_LOG={}", env::var("RUST_LOG").unwrap());
 
     return HttpServer::new(|| {
         App::new()
-            .wrap(Logger::default())
+            .wrap(middleware::Logger::default())
             .configure(routes::services)
     })
     .bind("127.0.0.1:8080")?
