1082c5164193b978a5162394779ea12599931fe8
23prime <23.prime.37@gmail.com>
2021-10-05 16:32:46 +0900

    Implement failed response for callback

--- src/controllers/callback.rs
+++ src/controllers/callback.rs
@@ -19,15 +19,29 @@ pub struct ResponseBody {
 
 #[get("/callback/")]
 pub async fn get(params: web::Query<Params>) -> impl Responder {
-    let access_token = token::fetch(params.code.clone())
-        .await
-        .unwrap()
-        .access_token;
-    let id = userinfo::fetch(&access_token).await.unwrap().sub;
+    let token_result = token::fetch(params.code.clone()).await;
+    if token_result.is_err() {
+        return failed_response();
+    }
+    let access_token = token_result.unwrap().access_token;
+
+    let userinfo_result = userinfo::fetch(&access_token).await;
+    if userinfo_result.is_err() {
+        return failed_response();
+    }
+    let id = userinfo_result.unwrap().sub;
+
     let after_login_url = env::var("AFTER_LOGIN_URL").expect("AFTER_LOGIN_URL must be set");
     let location = format!(
         "{}?id={}&access_token={}",
         after_login_url, id, access_token
     );
     return HttpResponse::Found().header(LOCATION, location).finish();
+}
+
+fn failed_response() -> HttpResponse {
+    let login_failed_url = env::var("LOGIN_FAILED_URL").expect("LOGIN_FAILED_URL must be set");
+    return HttpResponse::Found()
+        .header(LOCATION, login_failed_url)
+        .finish();
 }
