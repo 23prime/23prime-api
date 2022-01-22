6bd663d7cb502a1c4fda4e937cc42eea965c362e
23prime <23.prime.37@gmail.com>
2022-01-22 17:27:07 +0900

    Load DB URL only once

--- src/dbconfig.rs
+++ src/dbconfig.rs
@@ -2,8 +2,14 @@ use std::env;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use once_cell::sync::Lazy;
+
+static DATABASE_URL: Lazy<String> =
+    Lazy::new(|| env::var("DATABASE_URL").expect("DATABASE_URL must be set"));
 
 pub fn establish_connection() -> PgConnection {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    PgConnection::establish(&DATABASE_URL).expect(&format!(
+        "Error connecting to {}",
+        Lazy::force(&DATABASE_URL)
+    ))
 }
