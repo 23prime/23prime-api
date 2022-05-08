2926934cb4f8296090ff308ee980468609bacc06
23prime <23.prime.37@gmail.com>
2022-05-08 21:44:05 +0900

    Remove unnecessary mut

--- src/controllers/health_check/index.rs
+++ src/controllers/health_check/index.rs
@@ -12,10 +12,10 @@ mod tests {
 
     #[actix_rt::test]
     async fn get_test() {
-        let mut app = test::init_service(App::new().route("/", web::get().to(get))).await;
+        let app = test::init_service(App::new().route("/", web::get().to(get))).await;
 
         let req = test::TestRequest::get().uri("/").to_request();
-        let resp = test::call_service(&mut app, req).await;
+        let resp = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK);
 
         let body = actix_web::test::read_body(resp).await;
